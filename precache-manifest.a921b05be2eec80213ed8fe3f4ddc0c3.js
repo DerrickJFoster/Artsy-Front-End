98d7183588f43c4ae1b306178cde08b7e3b1ea5c
Derrick Foster <derrick.j.foster@gmail.com>
2019-06-29 10:55:12 -0600

    Updates

---
+++ precache-manifest.a921b05be2eec80213ed8fe3f4ddc0c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ad806a3aece01d6de45f9fcfb9acf8",
+    "url": "/Artsy-Front-End/index.html"
+  },
+  {
+    "revision": "32e72dfadb639e809940",
+    "url": "/Artsy-Front-End/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Artsy-Front-End/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "32e72dfadb639e809940",
+    "url": "/Artsy-Front-End/static/js/main.4435caec.chunk.js"
+  },
+  {
+    "revision": "e87f9622975e41a79672",
+    "url": "/Artsy-Front-End/static/js/runtime~main.f2e93413.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Artsy-Front-End/static/media/logo.5d5d9eef.svg"
+  }
+]);
