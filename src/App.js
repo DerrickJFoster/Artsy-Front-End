7eec9b66d741216499709f354cbdb04e87126407
barton-w <bartonw@gmail.com>
2019-06-29 13:23:21 -0600

    Hello world version

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import SearchArt from "./components/SearchArt.js"
+import "./css/normalize.css"
+import "./css/skeleton.css"
+import "./css/App.css";
 
 class App extends Component {
+  state = {
+    id: ""
+  }
   render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1>kickstART</h1>
+          <p>Find Inspiration And Spark Creativity</p>
+        </header>
+        <SearchArt />
+      </div>
+    );
+  }
 }
-}
+
 export default App;
