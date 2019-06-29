4c8c63919344dac813f72cd33bb9ac8eb7327f2e
barton-w <bartonw@gmail.com>
2019-06-29 13:23:43 -0600

    Updates

---
+++ precache-manifest.830d10a529037e16c5028e743cb793b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058afd431b062c2e5fd42a240934d57e",
+    "url": "/Artsy-Front-End/index.html"
+  },
+  {
+    "revision": "67c429a199e5cd57f018",
+    "url": "/Artsy-Front-End/static/css/main.5f678f51.chunk.css"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/Artsy-Front-End/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "67c429a199e5cd57f018",
+    "url": "/Artsy-Front-End/static/js/main.cc3c609b.chunk.js"
+  },
+  {
+    "revision": "e87f9622975e41a79672",
+    "url": "/Artsy-Front-End/static/js/runtime~main.f2e93413.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Artsy-Front-End/precache-manifest.a921b05be2eec80213ed8fe3f4ddc0c3.js"
+  "/Artsy-Front-End/precache-manifest.830d10a529037e16c5028e743cb793b1.js"
 );
 
 self.addEventListener('message', (event) => {
